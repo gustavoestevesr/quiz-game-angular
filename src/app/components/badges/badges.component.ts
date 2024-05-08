890083f1cdc4369d4941c27287aad03c1134370a
gustavoestevesr <gustavoesteves@directon.com.br>
2024-05-07 22:37:24 -0300

    ajuste de assets de emblemas

--- src/app/components/badges/badges.component.ts
+++ src/app/components/badges/badges.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CustomToolbarComponent } from '../custom-toolbar/custom-toolbar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { LISTA_EMBLEMAS } from '../../models/EmblemaModel.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-badges',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CustomToolbarComponent, MatCardModule, MatTooltipModule],
   templateUrl: './badges.component.html',
   styleUrl: './badges.component.scss'
 })
 export class BadgesComponent {
-
+  LISTA_EMBLEMAS = LISTA_EMBLEMAS;
 }
--- src/app/models/EmblemaModel.model.ts
+++ src/app/models/EmblemaModel.model.ts
@@ -2,18 +2,44 @@ import { QuizEnum } from '../enums/QuizEnum';
 
 export interface EmblemaModel {
   nome: string;
-  hablitado: boolean;
+  bloqueado: boolean;
   caminho: string;
 }
 
 export const LISTA_EMBLEMAS = [
   {
-    ditadura_militar_no_brasil: [
-      {
-        nome: QuizEnum.DITADURA_MILITAR_BRASIL,
-        hablitado: false,
-        caminho: './../../assets/emblemas/soldier.png',
-      },
-    ]
-  }
+    nome: QuizEnum.DITADURA_MILITAR_BRASIL,
+    bloqueado: true,
+    caminho: './../../assets/emblemas/soldado.png',
+  },
+  {
+    nome: QuizEnum.ERA_VARGAS,
+    bloqueado: true,
+    caminho: './../../assets/emblemas/presidente.png',
+  },
+  {
+    nome: QuizEnum.FEUDALISMO,
+    bloqueado: true,
+    caminho: './../../assets/emblemas/rei.png',
+  },
+  {
+    nome: QuizEnum.REVOLUCAO_FRANCESA,
+    bloqueado: true,
+    caminho: './../../assets/emblemas/revolucao-francesa.png',
+  },
+  {
+    nome: QuizEnum.REVOLUCAO_INDUSTRIAL_1,
+    bloqueado: true,
+    caminho: './../../assets/emblemas/fabrica.png',
+  },
+  {
+    nome: QuizEnum.REVOLUCAO_INDUSTRIAL_2,
+    bloqueado: true,
+    caminho: './../../assets/emblemas/braco-robotico.png',
+  },
+  {
+    nome: QuizEnum.MITOLOGIA_GREGA,
+    bloqueado: true,
+    caminho: './../../assets/emblemas/olimpo.png',
+  },
 ];
