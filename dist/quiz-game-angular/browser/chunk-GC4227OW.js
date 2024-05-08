a41f65c07e899dbc078dd67ba688a572b205a5f3
gustavoestevesr <gustavoesteves@directon.com.br>
2024-05-07 22:51:08 -0300

    build: deploy

---
+++ dist/quiz-game-angular/browser/chunk-GC4227OW.js
@@ -0,0 +1,1 @@
+import{ba as o,eb as i,fb as r,ub as a,yb as s}from"./chunk-CEPITATZ.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-highscore"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(i(0,"p"),a(1,"highscore works!"),r())}});let t=e;return t})();export{d as HighscoreComponent};
