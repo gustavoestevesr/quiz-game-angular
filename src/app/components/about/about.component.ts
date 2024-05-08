6967beeab8f89369d5d57f6167d7f12cc7529c80
gustavoestevesr <gustavoesteves@directon.com.br>
2024-05-07 21:00:43 -0300

    feat: adição da página sobre

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CustomToolbarComponent } from '../custom-toolbar/custom-toolbar.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CustomToolbarComponent, MatCardModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
