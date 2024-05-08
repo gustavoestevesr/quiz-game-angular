520b33416fdb57f59d7e1df4461def87b5161786
gustavoestevesr <gustavoesteves@directon.com.br>
2024-05-07 22:48:20 -0300

    build: deploy

--- dist/quiz-game-angular/browser/chunk-VQMEJ7VN.js
+++
@@ -1,1 +0,0 @@
-import{ba as o,eb as a,fb as i,ub as r,yb as s}from"./chunk-CEPITATZ.js";var m=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-badges"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,d){n&1&&(a(0,"p"),r(1,"badges works!"),i())}});let t=e;return t})();export{m as BadgesComponent};
