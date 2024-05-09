0396512aeea621f9e1d0518fe80ea6cb430a60f7
gustavoestevesr <gustavoesteves@directon.com.br>
2024-05-08 21:45:47 -0300

    feat: melhoria de responsividade com menu hamburguer em telas pequenas e toolbar em telas grandes

--- src/app/components/custom-toolbar/custom-toolbar.component.ts
+++ src/app/components/custom-toolbar/custom-toolbar.component.ts
@@ -1,17 +1,37 @@
-import { Router } from '@angular/router';
-import { Component, inject } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltip } from '@angular/material/tooltip';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-custom-toolbar',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatTooltip],
+  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule, MatTooltip, MatMenuModule],
   templateUrl: './custom-toolbar.component.html',
   styleUrl: './custom-toolbar.component.scss'
 })
 export class CustomToolbarComponent {
   public router = inject(Router);
+
+  isMobile: boolean;
+  menuLinks = [
+    { label: 'Inicio', route: 'inicio' },
+    { label: 'Emblemas', route: 'emblemas' },
+    { label: 'Ranking', route: 'ranking' },
+    { label: 'Autores', route: 'autores' },
+    { label: 'Sobre', route: 'sobre' }
+  ];
+
+  constructor() {
+    this.isMobile = window.innerWidth < 768;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.isMobile = window.innerWidth < 768;
+  }
 }
